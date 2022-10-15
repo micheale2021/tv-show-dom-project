746f8d026dd45256305d377a30ce015b9c2f420f
micheale2021 <andemariammicheale@gmail.com>
2022-10-16 00:17:27 +0100

    modify index.html

--- script.js
+++ script.js
@@ -7,6 +7,22 @@ function setup() {
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
   rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  const title = document.createElement("h1");
+  title.innerHTML = "TV SHOW";
+  title.style.textAlign = "center";
+  rootElem.appendChild(title);
+  for (let i = 0; i < episodeList.length; i++) {
+    const h3El = document.createElement("h3");
+    h3El.innerHTML = `${episodeList[i].name} - S${episodeList[i].season}E${episodeList[i].number}`;
+    const image = document.createElement("img")
+    image.setAttribute("src", `${episodeList[i].image.medium}`)
+    rootElem.appendChild(h3El)
+
+    
+  }
+    
+    
+  
 }
 
 window.onload = setup;
