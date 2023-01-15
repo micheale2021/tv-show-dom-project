273addec6d8a4ef9c2c258010bb8d3894b6c15a2
micheale2021 <andemariammicheale@gmail.com>
2023-01-15 21:50:09 +0000

    Add a select input that allows you to choose  show

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 setInterval(function () {
   changeColor();
-}, 4000);
+}, 6000);
 function changeColor() {
   var randomColor = Math.floor(Math.random() * 16777215).toString(16);
   document.body.style.backgroundColor = "#" + randomColor;
@@ -9,12 +9,23 @@ function changeColor() {
 const episodesList = document.getElementById("episodesList");
 const selectEl = document.getElementById("selectEpisode");
 
+const selectShowEl = document.getElementById("selectShow");
+
 const searchBar = document.getElementById("searchBar");
 let gotEpisodes = [];
 
 const displaySearchedEpisode = document.getElementById(
   "displaySearchedEpisode"
 );
+
+selectShowEl.addEventListener("change", (e) => {
+  const s = selectShowEl.value.toLowerCase();
+  console.log(s);
+  const filteredEpisode = gotEpisodes.filter((episode) => {
+    return episode.name.toLowerCase().includes("Winter");
+  });
+  displayShows(filteredEpisode);
+});
 
 selectEl.addEventListener("change", (e) => {
   const s = selectEl.value.toLowerCase();
@@ -46,9 +57,47 @@ const loadEpisodes = async () => {
   }
 };
 
+const loadShows = () => {
+  const gotShows = getAllShows();
+  displayShows(gotShows);
+};
+
+const displayShows = (shows) => {
+  const x = shows.length;
+  for (let i = 0; i < shows.length; i++) {
+    const optionEl = document.createElement("option");
+    optionEl.setAttribute("value", `${shows[i].name}`);
+    optionEl.innerHTML = `${shows[i].name}`;
+    selectShowEl.appendChild(optionEl);
+  }
+
+  //   displaySearchedEpisode.innerHTML = `Displaying ${episodes.length}/${gotEpisodes.length} episodes`;
+
+  //   const htmlString = episodes
+  //     .map((episode) => {
+  //       console.log(episode.number);
+  //       const seasonCode =
+  //         episode.season <= 9 ? `S0${episode.season}` : `S${episode.season}`;
+  //       const episodeCode =
+  //         episode.number <= 9 ? `E0${episode.number}` : `E${episode.number}`;
+
+  //       return `
+  //             <li class="episode">
+  //                 <h3>${episode.name}</h3>
+  // <h4> ${seasonCode}${episodeCode}</h4>
+  //            <p>${episode.summary}</p>
+  //                 <img src="${episode.image.medium}"></img>
+  //             </li>
+  //         `;
+  //     })
+  //     .join("");
+  //   episodesList.innerHTML = htmlString;
+  // optionEl.innerHTML = `${episodes.name}`;
+};
+
 const displayEpisodes = (episodes) => {
   const x = episodes.length;
-  console.log(episodes);
+  // console.log(episodes);
   for (let i = 0; i < episodes.length; i++) {
     const optionEl = document.createElement("option");
     optionEl.setAttribute("value", `${episodes[i].name}`);
@@ -86,7 +135,8 @@ const displayEpisodes = (episodes) => {
     })
     .join("");
   episodesList.innerHTML = htmlString;
-  optionEl.innerHTML = `${episodes.name}`;
+  // optionEl.innerHTML = `${episodes.name}`;
 };
 
 loadEpisodes();
+loadShows();
